e1caea7485b1e144cf4f71dd766a192562fe57f4
Yubo Tian <yubo.d.tian@gmail.com>
2015-02-24 20:34:01 -0500

    i/o bug fixed

--- main.c
+++ main.c
@@ -7,19 +7,38 @@
 //
 
 #include <stdio.h>
-#include <stdlib.g>
+#include <stdlib.h>
+#include <string.h>
+#include "ipsum.h"
 
 FILE *fp;
 
 int main(int argc, char* argv[]){
+    
     char* file = argv[1];
-    fp = fopen( file , "r");
-    char bar[1000];
-    while(1){
-        fscanf(fp,"%s\n", bar);
-        printf("%s", bar);
+    fp = fopen( argv[1] , "rt");
+    
+    char* line= NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+    if (fp == NULL){
+        printf("Opening file error\n");
+        exit(EXIT_FAILURE);
     }
     
+    while ((read = getline(&line, &len, fp)) != -1) {
+
+        char *pos;
+        if ((pos=strchr(line, '\n')) != NULL){
+            *pos = '\0';
+        }
+        printf("%s\n", line);
+        printf("next line\n");
+    }
     
-    fclose(file);
+
+    
+    
+    fclose(fp);
 }
