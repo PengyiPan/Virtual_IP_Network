d70a8a1ba461e5f03bc385d8fd7a0074b9d53be6
Yubo Tian <yubo.d.tian@gmail.com>
2015-02-27 00:50:51 -0500

    send and receive

--- main.cc
+++ main.cc
@@ -10,7 +10,18 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 #include <vector>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include "ipsum.h"
+
+//int server(uint16_t port);
+//int client(const char * addr, uint16_t port);
+
+#define MAX_MSG_LENGTH (1400)
+#define MAX_BACK_LOG (5)
+#define NUM_THREADS (9)
 
 using namespace std;
 
@@ -32,11 +43,191 @@ struct entry{
 struct interface_t{
 	int my_port;
 	int remote_port;
-	uint32_t my_VIP_addr;
-	uint32_t remote_VIP_addr;
+	char* my_VIP_addr;
+    char* remote_VIP_addr;
 };
 typedef struct interface_t interface;
 vector<interface*> my_interfaces(1);
+
+/*          *****************      from project 0             ****************************        */
+
+
+void * serverthread(void * parm);
+int server(uint16_t port);
+int client(const char * addr, uint16_t port);
+
+/*
+int main(int argc, char ** argv)
+{
+    if (argc < 3) {
+        printf("Command should be: myprog s <port> or myprog c <port> <address>\n");
+        return 1;
+    }
+    int port = atoi(argv[2]); //atoi covert string to int
+    if (port < 1024 || port > 65535) {
+        printf("Port number should be equal to or larger than 1024 and smaller than 65535\n");
+        return 1;
+    }
+    if (argv[1][0] == 'c') {
+        if(argv[3]==NULL){
+            printf("NO IP address is given\n");
+            return 1;
+        }
+        return client(argv[3], port);      //open client
+    } else if (argv[1][0] == 's') {
+        return server(port);               //open server
+    } else {
+        printf("unknown command type %s\nCommand should be: myprog s <port> or myprog c <port> <address>", argv[1]);
+        return 1;
+    }
+    return 0;
+}
+*/
+
+int client(const char * addr, uint16_t port)
+{
+    int sock;
+    struct sockaddr_in server_addr;
+    char msg[MAX_MSG_LENGTH], reply[MAX_MSG_LENGTH*3];
+    
+    if ((sock = socket(AF_INET, SOCK_STREAM/* use tcp */, 0)) < 0) {   //create socket and check error
+        perror("Create socket error:");
+        return 1;
+    }
+    
+    
+    printf("Socket created\n");
+    server_addr.sin_addr.s_addr = inet_addr(addr);  //sin_addr is struct in_addr
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connect error:");
+        return 1;
+    }
+    
+    printf("Connected to server %s:%d\n", addr, port);
+    
+    int recv_len = 0;
+    while (1) {
+        fflush(stdin);
+        printf("Enter message: \n");
+        gets(msg);
+        
+        
+        if (send(sock, msg, MAX_MSG_LENGTH, 0) < 0) {
+            perror("Send error:");
+            return 1;
+        }
+        
+        
+        recv_len = read(sock, reply, MAX_MSG_LENGTH*10);
+        
+        
+        if (recv_len < 0) {
+            perror("Recv error:");
+            return 1;
+        }
+        reply[recv_len] = 0;
+        printf("Server reply:\n%s\n", reply);
+        memset(reply, 0, sizeof(reply));
+    }
+    close(sock);
+    return 0;
+}
+
+int server(uint16_t port){
+    /*
+     add your code here
+     */
+    
+    int sock,cfd;
+    int len;
+    struct sockaddr_in sin;
+    char temp[MAX_MSG_LENGTH], reply[MAX_MSG_LENGTH*3];
+    int n;
+    pthread_t  tid;             // thread id
+    
+    //build address data structure?
+    
+    if ((sock = socket(AF_INET, SOCK_STREAM,0)) < 0) {
+        perror("Create socket error(server):");
+        return 1;
+    }
+    
+    printf("Socket created(server)\n");
+    
+    bzero(&sin, sizeof(sin)); //
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+    
+    
+    if (bind(sock,(struct sockaddr *)&sin, sizeof(sin))<0){
+        perror("Bind error:");
+        return 1;
+    }
+    
+    listen(sock,5);
+    
+    //    for(;;){
+    //        socklen_t len = sizeof(sin);
+    //        int cfd = accept(sock, (struct sockaddr*)&sin, &len);
+    //        printf("slides received");
+    //        fputs(reply, stdout);
+    //        close(cfd);
+    //    }
+    
+    
+    //wait for connection, then receive and print msg
+    while(1){
+        socklen_t len = sizeof(sin);
+        cfd = accept(sock, (struct sockaddr*)&sin, &len);
+        
+        //printf("after accept\n");
+        
+        if (cfd<0){
+            perror("Accept error:");
+            return 1;
+        }
+        
+        pthread_create(&tid, NULL, serverthread, (void *) cfd );
+        
+    }
+    close(sock);
+    return 0;
+}
+
+
+
+void * serverthread(void * parm){
+    int n;
+    char temp[MAX_MSG_LENGTH], reply[MAX_MSG_LENGTH*3];
+    int len;
+    int cfd;
+    
+    cfd = (int) parm;
+    
+    while(len = recv(cfd, temp, sizeof(temp), 0)){
+        //printf("Here is the message: %s\n",temp);
+        strcpy(reply,temp);
+        strcat(reply,temp);
+        strcat(reply,temp);
+        //printf("Here is the copy: %s\n",reply);
+        
+        n = write(cfd,reply,MAX_MSG_LENGTH*3);
+        
+        
+    }
+    close(cfd);
+    pthread_exit(0);
+    
+}
+
+/*          *****************      from project 0             ****************************        */
+
+ 
+
 
 
 void read_in(){
@@ -94,10 +285,11 @@ void read_in(){
 			interface* new_interface = (interface*) malloc(sizeof(interface));
 			new_interface-> my_port = my_port;
 			new_interface-> remote_port = temp_remote_port;
-			new_interface-> my_VIP_addr = 0; // should convert the temp_my_VIP_addr to uint32_t then store here
-			new_interface-> remote_VIP_addr = 0;
+			new_interface-> my_VIP_addr = temp_my_VIP_addr;
+            new_interface-> remote_VIP_addr = temp_remote_VIP_addr;
 
 			printf("remote_port in new interface is: %d \n", new_interface->remote_port);
+            printf("remote_VIP in new interface is: %s \n", new_interface->remote_VIP_addr);
 
 			my_interfaces.push_back(new_interface);
 
@@ -107,9 +299,6 @@ void read_in(){
 
 		}
 		line_count ++;
-
-
-
 
 	}
 
@@ -215,9 +404,6 @@ int main(int argc, char* argv[]){
 		else {
 			printf("Unrecognized command. Please retry.\n");
 		}
-
-
-
 
 
 	}
